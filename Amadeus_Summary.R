0c534629939d67e7ae3853b7f49d17880f7db00a
yangjh2612 <44026098+yangjh2612@users.noreply.github.com>
2018-10-19 15:37:03 +0100

    Update Amadeus_Summary.R

--- Amadeus_Summary.R
+++ Amadeus_Summary.R
@@ -8,7 +8,7 @@ country_names  <- c('Albania', 'Austria', 'Belarus', 'Belgium', 'Bosnia and Herz
 
 
 ## ------------------------------------------------------------------------
-# summary statistics 
+# summary statistics (Torsten)
 library(plyr)
 library(dplyr)
 library(zoo)
