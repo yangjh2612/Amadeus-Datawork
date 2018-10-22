2e0cee62e79820c05044ced250af70e8e9961cc7
Torsten <torsten.heinrich@posteo.net>
2018-10-22 18:23:26 +0100

    Removed obsolete plotting function

--- Regional_Plot_Scatter.R
+++ Regional_Plot_Scatter.R
@@ -1,25 +1,6 @@
 library(ggplot2)
 library(gridExtra)
 
-# plotting function (inconvenient and complicated; use the other below, if at all possible)
-plot_desc_stats_CP_vs_RoC_old <- function(desc_stats) {
-    plots <- list()
-    lpos = c('none','none','none','none','none','none','none','none','none','bottom')
-    for (year in 2006:2015) {
-        idx = year - 2005
-        year_results = desc_stats[desc_stats$Year==year, ]
-        #scatterplot(year_results$CP_median, year_results$RoC_median, reg.line=FALSE, xlab="Capital Productivity", ylab="Return on Total Assets")
-        plots[[idx]] <- ggplot(year_results, aes(CP_median, RoC_median, colour=Country)) + geom_point() + geom_rug(size=0.1) + theme(legend.title = element_blank(), legend.position=lpos[[idx]], text = element_text(size=10))
-    }
-    do.call(grid.arrange, c(plots, ncol=3))
-    ggsave(file = "Reg_NUTS_2_scatterplots_CP_vs_RoC.pdf", arrangeGrob(grobs = plots, ncol = 3))
-
-    #  ggplot(iris, aes(Sepal.Width, Petal.Width, colour=Species)) +
-    #    geom_point() +
-    #    geom_smooth(alpha=0.2) +
-    #    theme(legend.position="top"),
-}
-
 # plotting function
 plot_desc_stats_CP_vs_RoC <- function(desc_stats) {
     ggplot(desc_stats, aes(CP_median, RoC_median, colour=Country)) + geom_point(size=1,shape=".") + geom_rug(size=0.1) + theme(legend.title = element_blank(), legend.position="bottom") + facet_wrap(~Year) + coord_fixed(ratio=4) + labs(x="Capital Productivity", y="Return on Total Assets")
