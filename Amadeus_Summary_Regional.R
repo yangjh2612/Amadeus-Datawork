b018078afe4ccecd9c04ea564eaf967e0ef8183e
Torsten <torsten.heinrich@posteo.net>
2018-10-22 20:12:29 +0100

    Abstracted variable names in regional stats computation file

--- Amadeus_Summary_Regional.R
+++ Amadeus_Summary_Regional.R
@@ -16,7 +16,7 @@ num_obs <- function(dat, na.rm="ignored") {
 }
 
 # Compute descriptive statistics of capital productivity and profitability (returns on capital) by NUTS2 region
-desc_stat_by_file <- function(nuts_code, cfile, country) {
+desc_stat_by_file <- function(nuts_code, cfile, country, stat_variables = c("CP", "RoC")) {
     print(paste("Commencing", country, sep=" "))
     
     # load data file
@@ -28,24 +28,30 @@ desc_stat_by_file <- function(nuts_code, cfile, country) {
     }
     
     # remove what we do not need
-    rm(Cleaned_dat_cost_structure)
-    rm(Cleaned_dat_RD)
-    rm(Cleaned_dat_firm_size)
     Cleaned_dat_INDEX <- subset(Cleaned_dat_INDEX, select = c(IDNR, Year, get(nuts_code)))
-    Cleaned_dat_Profitability <- subset(Cleaned_dat_Profitability, select = c(IDNR, Year, RoC))
-    Cleaned_dat_Productivity <- subset(Cleaned_dat_Productivity, select = c(IDNR, Year, CP))
     
     # merge into one frame
     Cleaned_dat_INDEX <- merge(Cleaned_dat_INDEX, Cleaned_dat_Productivity, c("IDNR", "Year"))
+    rm(Cleaned_dat_Productivity)
     Cleaned_dat_INDEX <- merge(Cleaned_dat_INDEX, Cleaned_dat_Profitability, c("IDNR", "Year"))
-    Cleaned_dat_INDEX$IDNR <- NULL
+    rm(Cleaned_dat_Profitability)
+    Cleaned_dat_INDEX <- merge(Cleaned_dat_INDEX, Cleaned_dat_cost_structure, c("IDNR", "Year"))
+    rm(Cleaned_dat_cost_structure)
+    Cleaned_dat_INDEX <- merge(Cleaned_dat_INDEX, Cleaned_dat_firm_size, c("IDNR", "Year"))
+    rm(Cleaned_dat_firm_size)
+    Cleaned_dat_INDEX <- merge(Cleaned_dat_INDEX, Cleaned_dat_RD, c("IDNR", "Year"))
+    rm(Cleaned_dat_RD)
+    
+    retained_columns <- c("Year", nuts_code, stat_variables)
+    Cleaned_dat_INDEX <- subset(Cleaned_dat_INDEX, select = retained_columns)
+    colnames(Cleaned_dat_INDEX) <- c("Year", "nuts_code", stat_variables)
+    
     
     # compute statistics by region
     for(func in list("mean", "median", "sd", "entrop", "num_obs")) {
-        agg <-aggregate(.~get(nuts_code)+Year, Cleaned_dat_INDEX, FUN=func, na.rm=T, na.action=NULL)
-        agg <- agg[!(agg[,nuts_code] == ""),]
-        colnames(agg) <- c(nuts_code, "Year", paste("CP", func, sep="_"), paste("RoC", func, sep="_"))
-        #colnames(agg) <- c("NUTS_2", "Year", "CP_mean", "RoC_mean")
+        agg <-aggregate(.~nuts_code+Year, Cleaned_dat_INDEX, FUN=func, na.rm=T, na.action=NULL)
+        agg <- agg[!(agg[,"nuts_code"] == ""),]
+        colnames(agg) <- c(nuts_code, "Year", paste(stat_variables, func, sep="_"))
         if(exists("all_results")) {
             all_results <- merge(all_results, agg, c(nuts_code, "Year"))
         } else {
@@ -64,7 +70,7 @@ desc_stat_by_file <- function(nuts_code, cfile, country) {
 }
 
 # handle iteration over files list, call function to compute descriptive statistics for all, merge results
-desc_stat_all_files <- function (nuts_code, filenames, country_names) {
+desc_stat_all_files <- function (nuts_code, filenames, country_names, stat_variables = c("CP", "RoC")) {
 
     nfiles = length(filenames)
     
@@ -85,16 +91,16 @@ desc_stat_all_files <- function (nuts_code, filenames, country_names) {
     
 }
 
-nuts_level <- 2
+nuts_level <- 1
 nuts_code <- paste("NUTS", nuts_level, sep="_")
 
 filenames = c("panels_J!&Albania.Rda", "panels_J!&Austria.Rda", "panels_J!&Belarus.Rda", "panels_J!&Belgium.Rda", "panels_J!&Bulgaria.Rda", "panels_J!&Croatia.Rda", "panels_J!&Cyprus.Rda", "panels_J!&Czech Republic.Rda", "panels_J!&Denmark.Rda", "panels_J!&Estonia.Rda", "panels_J!&Finland.Rda", "panels_J!&France.Rda", "panels_J!&Germany.Rda", "panels_J!&Greece.Rda", "panels_J!&Hungary.Rda", "panels_J!&Iceland.Rda", "panels_J!&Ireland.Rda", "panels_J!&Italy.Rda", "panels_J!&Kosovo.Rda", "panels_J!&Latvia.Rda", "panels_J!&Liechtenstein.Rda", "panels_J!&Lithuania.Rda", "panels_J!&Luxembourg.Rda", "panels_J!&Malta.Rda", "panels_J!&Moldova.Rda", "panels_J!&Monaco.Rda", "panels_J!&Montenegro.Rda", "panels_J!&Netherlands.Rda", "panels_J!&Norway.Rda", "panels_J!&Poland.Rda", "panels_J!&Portugal.Rda", "panels_J!&Russian Federation.Rda", "panels_J!&Serbia.Rda", "panels_J!&Slovakia.Rda", "panels_J!&Spain.Rda", "panels_J!&Sweden.Rda", "panels_J!&Switzerland.Rda", "panels_J!&Turkey.Rda", "panels_J!&United Kingdom.Rda")
 country_names = c("Albania", "Austria", "Belarus", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Moldova", "Monaco", "Montenegro", "Netherlands", "Norway", "Poland", "Portugal", "Russian Federation", "Serbia", "Slovakia", "Spain", "Sweden", "Switzerland", "Turkey", "United Kingdom")
-#filenames = c("panels_J!&Austria.Rda", "panels_J!&Serbia.Rda")
-#country_names = c("Austria", "Serbia")
+filenames = c("panels_J!&Austria.Rda", "panels_J!&Serbia.Rda")
+country_names = c("Austria", "Serbia")
 
 desc_stats <- desc_stat_all_files(nuts_code, filenames, country_names)
-#print(desc_stats)
+print(desc_stats)
 output_file_name = paste(paste("Reg", nuts_code, sep="_"), "CP_RoC_desc_stats.Rda", sep="_")
 save(desc_stats, file=output_file_name)
 
