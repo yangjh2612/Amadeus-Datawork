37b2cbba133f867424b7903b3942ea50efeffdbf
Torsten <torsten.heinrich@posteo.net>
2019-01-16 19:37:25 +0000

    Shortened Prod_Fitting_Levy.R and Prof_Fitting_Levy_Boot.R and modified both to use new fittinglevy package

--- Prod_Fitting_Levy.R
+++ Prod_Fitting_Levy.R
@@ -1,123 +1,13 @@
-
-
 ############ 0. Basic Set up ############
 ## 0.1. loading of required libraries
-library(dplyr)
-library(StableEstim) # main package for the estimation of the Levy stable distributon
 
-##  0.2. loading of required data and cleaning
-load("All_list_Cleaned.Rda") ## load the data file created from "Productivity_Analysis_Data.Rmd"
+if (!'pacman' %in% installed.packages()[,'Package']) install.packages('pacman', repos='http://cran.r-project.org')
+pacman::p_load(dplyr,StableEstim,devtools)
 
-country_names <- c(
-  "Albania", "Austria", "Belarus", "Belgium", "Bosnia and Herzegovina",
-  "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia",
-  "Finland", "France", "Germany", "Greece", "Hungary", "Iceland", "Ireland",
-  "Italy", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg",
-  "Macedonia, FYR", "Malta", "Monaco", "Montenegro", "Netherlands", "Norway",
-  "Poland", "Portugal", "Moldova", "Romania", "Russian Federation", "Serbia",
-  "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Turkey", "Ukraine",
-  "United Kingdom"
-)
+## 0.2 loading fitting functions from fitting.levy package
+devtools::load_all("fittinglevy")
 
-country_names_five <- c("France", "Germany", "Italy", "Spain", "United Kingdom") # name of
-
-# Cleaning
-All_list_Cleaned_cut <- list()
-for (k in 1:length(All_list_Cleaned)) {
-  print(k)
-  All_list_Cleaned_cut[[k]] <- All_list_Cleaned[[k]] %>%
-    select(IDNR, Year, COMPCAT, NACE_CAT, LP, LP_g, EMPL, VA, Zeta) %>% # Firm ID, Year, Firm Size, Industry ID, Labor Produtivity, Labor Productivity Growth, Employment, Value added, TFP Growth
-    mutate(COMPCAT_one = substr(COMPCAT, 1, 1)) %>% # the first letter of the size variable
-    group_by(Year) %>%
-    filter(length(IDNR) > 10000) %>% # Note that 23 countries out of 44 do not have VA info. Out of 21 countries, 15 countries have enough info for anlaysis (more than 5 years with 10,000 firms )
-    group_by()
-}
-
-# Countries with more than 5 year obs with 10,000 firms
-take_this <- which(unlist(lapply(All_list_Cleaned_cut, function(x) length(unique(x$Year)) > 5)))
-
-All_list_Cleaned_cut <- All_list_Cleaned_cut[take_this]
-country_names <- country_names[take_this]
-
-rm(All_list_Cleaned)
-
-## 0.3. Setting the class names: Year, Size, Industry,
-# industry index
-
-ind_name_table <- data.frame(ind_names = unique(All_list_Cleaned_cut[[15]]$NACE_CAT), 
-                             ind_names_alphabet = c("J", "L", "G", "C", "I", "H", "S", "P", "M", "K", "B", "N", "A", "Q", "R", "F", "E", "O", "D", "T"))
-
-ind_name_table <- ind_name_table[order(ind_name_table$ind_names_alphabet),]
-
-size_names <- c("S", "M", "L", "V") # size index
-names(size_names) <- 1:4 # create the numerical name for the size index
-
-
-## 0.4. Setting of basic functions
-# Levy_fun_QT is the main function to estimate the Levy alpha stable distribution
-Levy_fun_QT <- function(x) {
-  ok <- McCullochParametersEstim(x)
-  return(ok)
-}
-
-Levy_fun_GMM <- function(x) {
-  pm <- 0
-  regularization = "cut-off"
-  WeightingMatrix = "OptAsym"
-  alphaReg = 0.005
-  algo = "2SGMM"
-  
-  t_scheme = "free"
-  t_seq = seq(0.1, 2, length.out = 12)
-  GMM_1 <- GMMParametersEstim(x = x,
-                              algo = algo, alphaReg = alphaReg,
-                              regularization = regularization,
-                              WeightingMatrix = WeightingMatrix,
-                              t_scheme = t_scheme,
-                              pm = pm, PrintTime = TRUE, t_free = t_seq)
-  
-}
-# Kullback–Leibler divergence
-kl_fun <- function(p, q) {
-  ifelse(p == 0, 0, p * log(p / q))
-}
-
-# Soofi ID
-soofi_gen <- function(pred_m, obs_m) {
-  soofi <- 1 - exp(-sum(kl_fun(pred_m, obs_m)))
-
-  return(soofi)
-}
-
-
-# Fittig function for the levy
-fun_info_gen <- function(dat_t, bin_num) { # two arguments: 1) data and 2) the bin number
-  p_data <- dat_t
-  est_levy_qt <- Levy_fun_QT(p_data) # Levy estimation with QT
-  #est_levy_gmm <- Levy_fun_GMM(p_data)
-  
-
-  p_data_h <- hist(p_data, plot = F, breaks = seq(min(p_data), max(p_data), l = bin_num)) # binning the data
-
-  obs_mid <- p_data_h$mids # location of the bin
-  obs_p <- p_data_h$counts / sum(p_data_h$counts) # normalized counts of the bin: the normalized empirical density
-
-  pred_p_levy_b <- dstable(obs_mid, est_levy_qt[1], est_levy_qt[2], est_levy_qt[3], est_levy_qt[4]) # the predicted density from the Levy model given the estimated parameters
-  
-  #pred_p_levy_b <- dstable(obs_mid, est_levy_gmm$Estim$par[1], est_levy_gmm$Estim$par[2], est_levy_gmm$Estim$par[3], est_levy_gmm$Estim$par[4]) # the predicted density from the Levy model given the estimated parameters
-  
-  pred_p_levy <- pred_p_levy_b / sum(pred_p_levy_b) # normalized density
-
-  levy_soofi <- 1 - soofi_gen(obs_p, pred_p_levy) # soofi index
-
-
-  ok_list <- list(raw_data = p_data, data_mid = obs_mid, data_p = obs_p, levy_q = pred_p_levy, levy_para = est_levy_qt, levy_soofi = round(levy_soofi, 4) * 100)
-  #ok_list <- list(raw_data = p_data, data_mid = obs_mid, data_p = obs_p, levy_q = pred_p_levy, levy_para = est_levy_gmm, levy_soofi = round(levy_soofi, 4) * 100)
-  
-  return(ok_list)
-}
-
-
+## 0.3 remaining function definitions: 
 # the fuction for the fitting result
 fun_fit_levy <- function(dat, bin_num, cond_ind, var_ind, c_names, cut_num, neg_cut, pov_cut) { # the function takes 8 arguments: 1) data generated and cleaned in section 0.2, 2) the number of bins, 3) the index of the variable that is used as the conditional class, 4) the index for target variable, 5) the name of class, 6) the minimum number of observations for each class,  7) the cutting point on the left tail, 8) the cutting point on the right tail
   result_list <- list()
@@ -177,7 +67,8 @@ fun_fit_levy <- function(dat, bin_num, cond_ind, var_ind, c_names, cut_num, neg_
         print(paste(length(c_uni), c))
         c_lp <- zz$Var[zz$Cond == c_uni_name[c]] # for each class
 
-        c_list[[c]] <- fun_info_gen(dat_t = c_lp, bin_num = bin_num) # Levy estimation
+        levy_result <- levy_fitting(dat_t = c_lp, bin_num = bin_num, include_bootstrap=FALSE) # Levy estimation
+        c_list[[c]] <- list(raw_data = levy_result$raw_data, data_mid = levy_result$data_mid, data_p = levy_result$data_p, levy_q = levy_result$levy_q, levy_para = levy_result$levy_para, levy_soofi = levy_result$levy_soofi)
       }
       c_uni_list[[k]] <- c_uni_name # record the ordered name of unique class
       c_uni_list_2[[k]] <- c_uni_name_2 #
@@ -190,7 +81,59 @@ fun_fit_levy <- function(dat, bin_num, cond_ind, var_ind, c_names, cut_num, neg_
 }
 
 
-############ 1. Fit the Levy distribution  ############
+# main entry point
+
+##  1.1. loading of required data and cleaning
+load("All_list_Cleaned.Rda") ## load the data file created from "Productivity_Analysis_Data.Rmd"
+
+country_names <- c(
+  "Albania", "Austria", "Belarus", "Belgium", "Bosnia and Herzegovina",
+  "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia",
+  "Finland", "France", "Germany", "Greece", "Hungary", "Iceland", "Ireland",
+  "Italy", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg",
+  "Macedonia, FYR", "Malta", "Monaco", "Montenegro", "Netherlands", "Norway",
+  "Poland", "Portugal", "Moldova", "Romania", "Russian Federation", "Serbia",
+  "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Turkey", "Ukraine",
+  "United Kingdom"
+)
+
+country_names_five <- c("France", "Germany", "Italy", "Spain", "United Kingdom") # name of
+
+# Cleaning
+All_list_Cleaned_cut <- list()
+for (k in 1:length(All_list_Cleaned)) {
+  print(k)
+  All_list_Cleaned_cut[[k]] <- All_list_Cleaned[[k]] %>%
+    select(IDNR, Year, COMPCAT, NACE_CAT, LP, LP_g, EMPL, VA, Zeta) %>% # Firm ID, Year, Firm Size, Industry ID, Labor Produtivity, Labor Productivity Growth, Employment, Value added, TFP Growth
+    mutate(COMPCAT_one = substr(COMPCAT, 1, 1)) %>% # the first letter of the size variable
+    group_by(Year) %>%
+    filter(length(IDNR) > 10000) %>% # Note that 23 countries out of 44 do not have VA info. Out of 21 countries, 15 countries have enough info for anlaysis (more than 5 years with 10,000 firms )
+    group_by()
+}
+
+# Countries with more than 5 year obs with 10,000 firms
+take_this <- which(unlist(lapply(All_list_Cleaned_cut, function(x) length(unique(x$Year)) > 5)))
+
+All_list_Cleaned_cut <- All_list_Cleaned_cut[take_this]
+country_names <- country_names[take_this]
+
+rm(All_list_Cleaned)
+
+## 1.2. Setting the class names: Year, Size, Industry,
+# industry index
+
+year_names <- c(2006:2015) 
+
+ind_name_table <- data.frame(ind_names = unique(All_list_Cleaned_cut[[15]]$NACE_CAT), 
+                             ind_names_alphabet = c("J", "L", "G", "C", "I", "H", "S", "P", "M", "K", "B", "N", "A", "Q", "R", "F", "E", "O", "D", "T"))
+
+ind_name_table <- ind_name_table[order(ind_name_table$ind_names_alphabet),]
+
+size_names <- c("S", "M", "L", "V") # size index
+names(size_names) <- 1:4 # create the numerical name for the size index
+
+
+############ 2. Fit the Levy distribution  ############
 ## note the following index
 # con_ind: 2: year, 3: size, 4: industry
 # var_ind: 5: LP, 6: LP_change, 7: TFP growth
