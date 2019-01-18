561aa073827cdb5079bf9df16c527dfd821cd83b
Julian Winkler <JulianW@oxfordmartin.ox.ac.uk>
2019-01-18 14:22:07 +0000

    Added comments

--- sim_levy_norm.R
+++ sim_levy_norm.R
@@ -1,7 +1,7 @@
-rm(list = ls())
-gc()
+rm(list = ls()) # clear environment
+gc() # garbage collection
 
-pacman::p_load(stabledist,tidyverse,foreach,doParallel,reshape)
+pacman::p_load(stabledist,tidyverse,foreach,doParallel,reshape,xtable) # load packages
 
 # n <- 1000000
 # b <- n/10
@@ -20,52 +20,52 @@ pacman::p_load(stabledist,tidyverse,foreach,doParallel,reshape)
 
 
 # Using Variance
-if(!file.exists('C:/Users/JulianW/Documents/Work/Productivity Dispersion/testsd.Rda')){
-  alpha <- 1.7
+if(!file.exists('C:/Users/JulianW/Documents/Work/Productivity Dispersion/testsd.Rda')){ # I run the simulations once, then save the result, because it takes too long to run
+  alpha <- 1.7 # Levy parameters
   beta <- 0
   gamma <- 1
   delta <- 0
-  n <- 8
-  runs <- iter(1:1000)
+  n <- 8 # how many samples sizes do you want to consider? 10^n
+  runs <- iter(1:1000) # how many interations of each sample size
   
-  cl <- makePSOCKcluster(detectCores())
+  cl <- makePSOCKcluster(detectCores()) # register parallel backend
   registerDoParallel(cl)
   
-  start_time <- Sys.time()
-  testsd <- foreach(i = runs, .combine = rbind, .packages = 'stabledist') %dopar% {
-    c <- as.list(seq(1,n,1)) 
+  start_time <- Sys.time() # time the code
+  testsd <- foreach(i = runs, .combine = rbind, .packages = 'stabledist') %dopar% { # foreach loop the returns one set of simulated samples, of size 1 to n
+    c <- as.list(seq(1,n,1)) # vector sample sizes
     c <- lapply(c, function(x) {
-      sd(rstable(10^x, alpha, beta, gamma, delta, pm = 0))
+      sd(rstable(10^x, alpha, beta, gamma, delta, pm = 0)) # standard deviation of the sample of draws from the levy
     })
-    unlist(c)
+    unlist(c) # return the vector, which foreach combines into a df
   }
   end_time <- Sys.time()
-  stopCluster(cl)
+  stopCluster(cl) # close the backend
   time <- end_time - start_time # 1h48m
   
-  save(testsd, file = 'C:/Users/JulianW/Documents/Work/Productivity Dispersion/testsd.Rda')
+  save(testsd, file = 'C:/Users/JulianW/Documents/Work/Productivity Dispersion/testsd.Rda') # save results
   rm(alpha, beta, gamma, delta, i, start_time, end_time, time)
 } else {
-  load('C:/Users/JulianW/Documents/Work/Productivity Dispersion/testsd.Rda')
+  load('C:/Users/JulianW/Documents/Work/Productivity Dispersion/testsd.Rda') # if the results exist already, just load them to save time
 }
 
-sd_sum <- as.data.frame(testsd)
-sd_sum <- melt(sd_sum)
-class(sd_sum$variable) <- 'character'
-sd_sum <- sd_sum %>% 
+sd_sum <- as.data.frame(testsd) # create DF
+sd_sum <- melt(sd_sum) # reshape into long format
+class(sd_sum$variable) <- 'character' # turn into character
+sd_sum <- sd_sum %>% # take the relevant statistics
   group_by(variable) %>%
   mutate(mean = mean(value),
          median = median(value),
          topquint = quantile(value, 0.95),
          botquint = quantile(value, 0.05))
 
-sd_sum_srt <- sd_sum %>%
+sd_sum_srt <- sd_sum %>% # keep the statistics we are interested in
   group_by(variable) %>%
   slice(1) %>%
   ungroup() %>%
   select(-value)
   
-
+# Plot the standard deviations
 pdf('C:/Users/JulianW/Documents/Work/Productivity Dispersion/sample_std_dev_alpha_17.pdf', height = 6, width = 8)
 par(mfcol = c(1,1))
 
@@ -98,7 +98,9 @@ legend('topleft',
 
 dev.off()
 
-iqr_tab <- as.data.frame(rbind(c(0.95,0.05,1.7,0,1,0),
+# IQR as measure of dispersion
+
+iqr_tab <- as.data.frame(rbind(c(0.95,0.05,1.7,0,1,0), # create the parameters for the relevant Levy distributions
                                c(0.95,0.05,1.5,0,1,0),
                                c(0.9,0.1,1.7,0,1,0),
                                c(0.9,0.1,1.5,0,1,0),
@@ -111,9 +113,12 @@ iqr_tab <- as.data.frame(rbind(c(0.95,0.05,1.7,0,1,0),
                                c(0.75,0.25,1.5,0,1,0),
                                c(0.75,0.25,1.5,0.5,1,0)))
 colnames(iqr_tab) <- c('Top', 'Bottom', 'a', 'b', 'g', 'd')
+
 iqr_tab <- iqr_tab %>%
   rowwise() %>%
-  mutate(IQR = qstable(Top, a, b, g, d) - qstable(Bottom, a, b, g, d)) %>%
+  mutate(IQR = qstable(Top, a, b, g, d) - qstable(Bottom, a, b, g, d)) %>% # find the relevant IQRs for the given parameters
   ungroup() %>%
-  mutate(pc_IQR = ifelse((row_number() %% 2) == 0, (IQR - lag(IQR,1))/lag(IQR,1), NA))
+  mutate(pc_IQR = ifelse((row_number() %% 2) == 0, (IQR - lag(IQR,1))/lag(IQR,1), NA),
+         pc_IQR = round(100*pc_IQR, digit = 2)) # express as %
 
+print(xtable(iqr_tab, caption = 'Interquartile Range as a Measure of Dispersion'), include.rownames = FALSE) # paste table into latex
