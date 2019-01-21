ff5029a89febc3f14f4d7b4ee85c40dc0ba0fe83
Julian Winkler <JulianW@oxfordmartin.ox.ac.uk>
2019-01-21 10:02:31 +0000

    Seperated standard deviation and iqr, modified comment on line 23, description at start

--- sim_levy_norm.R
+++ sim_levy_norm.R
@@ -1,3 +1,5 @@
+# This file runs the simulation for standard deviations
+
 rm(list = ls()) # clear environment
 gc() # garbage collection
 
@@ -18,9 +20,7 @@ pacman::p_load(stabledist,tidyverse,foreach,doParallel,reshape,xtable) # load pa
 # hist(norm_cut, breaks = b)
 # mtext(paste("n = ", n, sep = ''), side = 3, line = -25, outer = TRUE)
 
-
-# Using Variance
-if(!file.exists('C:/Users/JulianW/Documents/Work/Productivity Dispersion/testsd.Rda')){ # I run the simulations once, then save the result, because it takes too long to run
+if(!file.exists('C:/Users/JulianW/Documents/Work/Productivity Dispersion/testsd.Rda')){ # Do not run the simulation again if previous results are saved
   alpha <- 1.7 # Levy parameters
   beta <- 0
   gamma <- 1
@@ -97,28 +97,3 @@ legend('topleft',
        lty = c(1, 2), lwd = 2)
 
 dev.off()
-
-# IQR as measure of dispersion
-
-iqr_tab <- as.data.frame(rbind(c(0.95,0.05,1.7,0,1,0), # create the parameters for the relevant Levy distributions
-                               c(0.95,0.05,1.5,0,1,0),
-                               c(0.9,0.1,1.7,0,1,0),
-                               c(0.9,0.1,1.5,0,1,0),
-                               c(0.75,0.25,1.7,0,1,0),
-                               c(0.75,0.25,1.5,0,1,0),
-                               c(0.95,0.05,1.5,0,1,0),
-                               c(0.95,0.05,1.5,0.5,1,0),
-                               c(0.9,0.1,1.5,0,1,0),
-                               c(0.9,0.1,1.5,0.5,1,0),
-                               c(0.75,0.25,1.5,0,1,0),
-                               c(0.75,0.25,1.5,0.5,1,0)))
-colnames(iqr_tab) <- c('Top', 'Bottom', 'a', 'b', 'g', 'd')
-
-iqr_tab <- iqr_tab %>%
-  rowwise() %>%
-  mutate(IQR = qstable(Top, a, b, g, d) - qstable(Bottom, a, b, g, d)) %>% # find the relevant IQRs for the given parameters
-  ungroup() %>%
-  mutate(pc_IQR = ifelse((row_number() %% 2) == 0, (IQR - lag(IQR,1))/lag(IQR,1), NA),
-         pc_IQR = round(100*pc_IQR, digit = 2)) # express as %
-
-print(xtable(iqr_tab, caption = 'Interquartile Range as a Measure of Dispersion'), include.rownames = FALSE) # paste table into latex
